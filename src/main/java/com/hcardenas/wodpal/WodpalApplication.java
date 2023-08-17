4a348599c6febb6e8490ce649716b3f475067a34
helmutcardenas <helmut.cardenas@improving.com>
2023-08-17 11:19:37 -0500

    init

---
+++ src/main/java/com/hcardenas/wodpal/WodpalApplication.java
@@ -0,0 +1,13 @@
+package com.hcardenas.wodpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WodpalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcardenas/wodpal/WodpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcardenas.wodpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodpalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
